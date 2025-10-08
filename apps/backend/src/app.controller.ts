68f6636f83d36ff99f9c621bfdbf3aa6d0c4eb45
Dominik Schulz <dominik@dschulz.dev>
2025-10-08 17:51:22 +0200

    fix(app-controller): add AllowAnonymous decorator to getHello method

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from "@nestjs/common";
+import { AllowAnonymous } from "@thallesp/nestjs-better-auth";
 
 @Controller()
 export class AppController {
 	@Get("")
+	@AllowAnonymous()
 	public getHello(): string {
 		return "Hello World!";
 	}
